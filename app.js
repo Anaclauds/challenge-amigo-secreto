29b15ef7594956779c7c938742c28052797ee715
Ana Claudia <a.claudia@estudante.ifro.edu.br>
2025-03-13 22:32:59 -0400

    Criaçãod e array para lista de nomes e implementação da função para adicionar um novo amiho

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//Array para armazenar nomes digitados
+let listaDeNomes = []
+
+//Função para adicionar amigos na lista
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim(); 
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");  
+    } else {
+        listaDeNomes.push(nome); 
+        input.value = ""; 
+        
+    }
+    
+//teste:
+    console.log(listaDeNomes); 
+}
